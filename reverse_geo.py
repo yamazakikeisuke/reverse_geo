39b45d17dca2de58da55c78437618428a1655280
keisuke_z <keisukedj9@gmail.com>
2023-09-06 18:52:26 +0900

    Add files via upload

--- reverse_geo.py
+++ reverse_geo.py
@@ -5,7 +5,7 @@ from shapely.geometry import Polygon, Point
 def reverse_geo(shdf, point):
     for index, row in shdf.iterrows():
        polygon = row['geometry']
-       #"point"がどの境界に含まれるかを判定する
+       #"point"がどのポリゴンに含まれるかを判定する
        if polygon.contains(point):
           #見つかったら該当する市区町村の情報を返す
           return(row)
@@ -18,10 +18,9 @@ shdf = gp.read_file('./japan_ver84/japan_ver84.shp', encoding='shift-jis')
 #座標系の確認。調べたい地点の座標系と同一であることを必ず確認する
 print(shdf.crs)
 
-#調べたい地点の情報（例）
-position = Point(139.45802792168061, 35.318270525221962)
+#調べたい地点の情報（例） 
+position = Point(139.752852, 35.684204)
 
 #逆ジオコーディングを実行
 reverse_geo_info = reverse_geo(shdf, position)
 print(reverse_geo_info)
-
